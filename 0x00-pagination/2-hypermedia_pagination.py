8c59b60549dc71f3e1336e1d9c14741b83bc8454
bereket-kume <bereketkume@gmail.com>
2024-08-17 23:17:56 +0300

    third task

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import csv
 import math
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple:
@@ -17,7 +17,7 @@ def index_range(page: int, page_size: int) -> Tuple:
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
+    DATA_FILE = "7d3576d97e7560ae85135cc214ffe2b3412c51d7(1).csv"
 
     def __init__(self):
         self.__dataset = None
@@ -37,10 +37,27 @@ class Server:
         """
         function that return file from csv file
          """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
         if start_index > len(dataset):
             return []
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        function that return file from csv file
+        Hypermedia pagination
+        """
+        val = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_page = math.ceil(total_items / page_size)
+        return {
+            'page_size': len(val),
+            'page': page,
+            'data': val,
+            'next_page': page + 1 if page < len(self.__dataset) else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_page
+        }
