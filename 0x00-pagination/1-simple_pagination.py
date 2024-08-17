2e35ce4e5d0b48527defeebca42a00e74a536e17
bereket-kume <bereketkume@gmail.com>
2024-08-17 22:43:00 +0300

    error

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,9 +9,7 @@ def index_range(page: int, page_size: int) -> Tuple:
     function that takes two integer argument
     return start index and end index
     """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return (start_index, end_index)
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
