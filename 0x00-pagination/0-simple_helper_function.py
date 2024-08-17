bac2deff5fafe0640f62e2a46862556cb288e7cd
bereket-kume <bereketkume@gmail.com>
2024-08-17 18:58:15 +0300

    error fixed

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,13 +2,14 @@
 """
 Index Range
 """
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple:
     """
     function that takes two integer argument
     return start index and end index
     """
     start_index = (page - 1) * page_size
-    end_index = page* page_size
+    end_index = page * page_size
     return (start_index, end_index)
