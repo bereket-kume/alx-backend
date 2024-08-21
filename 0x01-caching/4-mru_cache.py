262ee47538c11b322ac38d1069aae4ad07e28831
bereket-kume <bereketkume@gmail.com>
2024-08-21 18:40:28 +0300

    error fixed

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -17,11 +17,14 @@ class MRUCache(BaseCaching):
         """
         super().__init__()
         self.cache_data = OrderedDict()
-    
+
     def put(self, key, item):
         """
         Add or Update an item in the cache
         """
+        if key is None or item is None:
+            return
+
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 discard, _ = self.cache_data.popitem(False)
