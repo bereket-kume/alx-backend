1445b73f7766bd600a58ceeb27867a7cc6516188
bereket-kume <bereketkume@gmail.com>
2024-08-21 18:38:04 +0300

    MRU caching system task

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+MRU Caching system
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU caching class
+    implements an MRU caching mechanism
+    """
+    def __init__(self):
+        """
+        Initialize the MRUCache instance
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+    
+    def put(self, key, item):
+        """
+        Add or Update an item in the cache
+        """
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                discard, _ = self.cache_data.popitem(False)
+                print(f"DISCARD: {discard}")
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
