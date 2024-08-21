5af98494cd38848bf77b39858ee3427d29565779
bereket-kume <bereketkume@gmail.com>
2024-08-21 23:02:57 +0300

    LFU Caching system Task

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+LFU Caching System
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict, defaultdict
+
+
+class LFUCache(BaseCaching):
+    """
+    LFU Caching class
+    Implements an LFU Caching mechanism
+    """
+    def __init__(self):
+        """
+        Initialize the LFUCache instance
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.freq_count = {}
+
+    def put(self, key, item):
+        """
+        Add Or update an item in the cache
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.freq_count[key] += 1
+            self.cache_data.move_to_end(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                min_freq = min(self.freq_count.values())
+                lfu_keys = [
+                    k for k, v in self.freq_count.items()
+                    if v == min_freq
+                    ]
+
+                for k in self.cache_data:
+                    if k in lfu_keys:
+                        print(f"DISCARD: {k}")
+                        del self.cache_data[k]
+                        del self.freq_count[k]
+                        break
+            self.cache_data[key] = item
+            self.freq_count[key] = 1
+            self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            self.freq_count[key] += 1
+            return self.cache_data[key]
+        return None
