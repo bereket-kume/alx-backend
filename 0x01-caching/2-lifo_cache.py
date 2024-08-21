ad8aa820500141d49d3977c05d96f6effdc42fd8
bereket-kume <bereketkume@gmail.com>
2024-08-21 17:34:06 +0300

    error in the put function fixed

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -27,7 +27,7 @@ class LIFOCache(BaseCaching):
 
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                discard, _ = self.cache_data.popitem()
+                discard, _ = self.cache_data.popitem(True)
                 print(f"DISCARD {discard}")
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
