c05f7f6d2566b6cce4bc4597a507000e9dfd9508
bereket-kume <bereketkume@gmail.com>
2024-08-27 13:45:51 +0300

    error fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+"""
+Basic flask app
+"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """
+    render index.html file form template
+    """
+    return render_template('0-index.html')
 
-if __name__ ==  "__main__":
-    app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
