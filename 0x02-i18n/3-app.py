c0054e7a02e365972fd44df3c652a4ab19afa7a0
bereket-kume <bereketkume@gmail.com>
2024-08-27 16:54:22 +0300

    error fixed

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -20,12 +20,14 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale():
     """
     get_locale function with @babel.localeselector
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/")
